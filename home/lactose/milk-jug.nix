a57ade7c276224c34491d7294f7ba4c4c633d813
lactose <juuyokka@users.noreply.github.com>
2024-01-30 02:50:43 -0800

    Edit home config

--- home/lactose/milk-jug.nix
+++ home/lactose/milk-jug.nix
@@ -5,6 +5,34 @@
     stateVersion = "23.11";
   };
 
-  milky.hyprland.enable = true;
-  milky.git.enable = true;
+  milky = {
+    foot.enable = true;
+    git.enable = true;
+    hyprland.enable = true;
+    neovim.enable = true;
+    vesktop.enable = true;
+
+    firefox.enable = true;
+    firefox.bookmarks = [
+      {
+        name = "NixOS";
+        toolbar = true;
+        bookmarks = [
+          {
+            name = "NixOS Packages";
+            url = "https://search.nixos.org/packages";
+          }
+          {
+            name = "NixOS Options";
+            url = "https://search.nixos.org/options";
+          }
+          {
+            name = "NixOS Wiki";
+            url = "https://nixos.wiki";
+          }
+        ];
+      }
+    ];
+
+  };
 }
