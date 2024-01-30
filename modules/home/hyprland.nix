05387badc4d12ed9ffbb2b25b7cd5681107d0166
lactose <juuyokka@users.noreply.github.com>
2024-01-30 02:51:07 -0800

    Add basic Hyprland config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -8,7 +8,12 @@ let
     mkEnableOption
     mkIf
     mkOption
+    range
     types
+    zipLists
+    ;
+  inherit (builtins)
+    toString
     ;
 
   cfg = config.milky.hyprland;
@@ -16,6 +21,10 @@ in
 {
   options.milky.hyprland = {
     enable = mkEnableOption "Enable Hyprland";
+    modKey = mkOption {
+      type = types.str;
+      default = "SUPER";
+    };
     system = mkOption {
       type = types.str;
       default = "x86_64-linux";
@@ -29,8 +38,41 @@ in
       xwayland.enable = true;
       enable = true;
 
-      # TODO: do some cool stuff here!
-      settings = { };
+      settings =
+        let
+          mod = cfg.modKey;
+          workspaceBindings =
+            let
+              fn = x: "${mod},${toString x.fst},workspace,${toString x.snd}";
+              rn = range 1 9;
+              ls = (zipLists rn rn) ++ [{ fst = 0; snd = 10; }];
+            in
+            map fn ls;
+        in
+        {
+          bind = [
+            "${mod},KP_Enter,exec,foot"
+            "${mod}+SHIFT,Q,killactive"
+            "${mod},C,exec,vencorddesktop"
+
+            "${mod},H,movefocus,l"
+            "${mod},J,movefocus,d"
+            "${mod},K,movefocus,u"
+            "${mod},L,movefocus,r"
+          ] ++ workspaceBindings;
+
+          general = {
+            layout = "master";
+          };
+
+          misc = {
+            disable_hyprland_logo = true;
+            disable_splash_rendering = true;
+            force_default_wallpaper = 0;
+            enable_swallow = true;
+            swallow_regex = "^foot(client)?$";
+          };
+        };
     };
   };
 }
