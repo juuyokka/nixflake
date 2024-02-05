53cedae622af9a12896e0acb918efbd11173a7ee
lactose <juuyokka@users.noreply.github.com>
2024-02-05 04:47:57 -0800

    Change partition scheme

--- hosts/milk-jug/disko.nix
+++ hosts/milk-jug/disko.nix
@@ -1,45 +1,47 @@
 {
   disko.devices = {
     disk = {
-      main = {
-        device = "/dev/disk/by-id/nvme-Sabrent_SB-ROCKET-NVMe4-1TB_48790459505232";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "500M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              type = "8300";
-              size = "160G";
-              content = {
-                type = "filesystem";
-                format = "xfs";
-                extraArgs = [ "-f" ];
-                mountpoint = "/";
-              };
-            };
-            nix = {
-              type = "8300";
-              size = "128G";
-              content = {
-                type = "filesystem";
-                format = "btrfs";
-                extraArgs = [ "-f" ];
-                mountOptions = [ "compress=zstd:15" "nodatacow" ];
-                mountpoint = "/nix";
-              };
-            };
-          };
-        };
-      };
+			boot = {
+				type = "disk";
+				device = "/dev/disk/by-id/nvme-CT1000T700SSD3_2339E87A058B";
+				content {
+					type = "gpt";
+					partitions = {
+						ESP = {
+							type = "EF00";
+							size = "500M";
+							content = {
+								type = "filesystem";
+								format = "vfat";
+								mountpoint = "/boot";
+							};
+						};
+						root = {
+							type = "8E00";
+							size = "100%";
+							content = {
+								type = "lvm_pv";
+								vg = "root";
+							};
+						};
+					};
+				};
+			};
     };
+		lvm_vg = {
+			root = {
+				type = "lvm_vg";
+				lvs = {
+					root = {
+						type = "8300";
+						size = "100%";
+						content = {
+							type = "filesystem";
+							format = "/";
+						};
+					};
+				};
+			};
+		};
   };
 }
