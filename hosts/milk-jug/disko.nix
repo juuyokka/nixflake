c1e7b20e31ddfe422527a02f9ffad3a91bc0ffac
lactose <15185244+juuyokka@users.noreply.github.com>
2024-02-05 23:29:38 +0000

    Fix disko

--- hosts/milk-jug/disko.nix
+++ hosts/milk-jug/disko.nix
@@ -1,47 +1,48 @@
 {
   disko.devices = {
     disk = {
-			boot = {
-				type = "disk";
-				device = "/dev/disk/by-id/nvme-CT1000T700SSD3_2339E87A058B";
-				content = {
-					type = "gpt";
-					partitions = {
-						ESP = {
-							type = "EF00";
-							size = "500M";
-							content = {
-								type = "filesystem";
-								format = "vfat";
-								mountpoint = "/boot";
-							};
-						};
-						root = {
-							type = "8E00";
-							size = "100%";
-							content = {
-								type = "lvm_pv";
-								vg = "root";
-							};
-						};
-					};
-				};
-			};
+      boot = {
+        type = "disk";
+        device = "/dev/disk/by-id/nvme-CT1000T700SSD3_2339E87A058B";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+		            mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              type = "8E00";
+              size = "100%";
+              content = {
+                type = "lvm_pv";
+                vg = "vgroot";
+              };
+            };
+          };
+        };
+      };
     };
-		lvm_vg = {
-			root = {
-				type = "lvm_vg";
-				lvs = {
-					root = {
-						size = "100%";
-						content = {
-							type = "filesystem";
-							format = "xfs";
-							mountpoint = "/";
-						};
-					};
-				};
-			};
-		};
+    lvm_vg = {
+      vgroot = {
+        type = "lvm_vg";
+        lvs = {
+          rootfs = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
   };
 }
