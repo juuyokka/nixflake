78cb20151af05af02ee3f8c69b9f92b3cfda9ece
lactose <15185244+juuyokka@users.noreply.github.com>
2024-02-05 23:31:05 +0000

    Fix hyprland-portals module

--- modules/nixos/hyprland-portals.nix
+++ modules/nixos/hyprland-portals.nix
@@ -1,30 +1,30 @@
-{ config, lib, inputs, self, ... }:
+{ config, lib, inputs, self, pkgs, ... }:
 let
   inherit (lib)
-		mkEnableOption
-		mkIf
-		mkOption
-		types
-		;
+    mkEnableOption
+    mkIf
+    mkOption
+    types
+    ;
 
-		cfg = config.milky.hyprland-portals;
+  cfg = config.milky.hyprland-portals;
 in
 {
-	option.milky.hyprland-portals = {
-		enable = mkEnableOption "Enable xdg-desktop-portals for Hyprland";
-		system = mkOption {
+  options.milky.hyprland-portals = {
+    enable = mkEnableOption "Enable xdg-desktop-portals for Hyprland";
+    system = mkOption {
       type = types.str;
       default = "x86_64-linux";
     };
-	};
+  };
 
-	config = mkIf cfg.enable rec {
-		xdg.portal = {
-			enable = true;
-			extraPortals = with pkgs; [
-					inputs.hyprland.inputs.xdph.packages.${cfg.system}.default
-					xdg-desktop-portal-gtk
-			];
-		};
-	};
+  config = mkIf cfg.enable rec {
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        inputs.hyprland.inputs.xdph.packages.${cfg.system}.default
+        xdg-desktop-portal-gtk
+      ];
+    };
+  };
 }
